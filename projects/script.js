72ba31f50edcd3224cafc7668808ff516c80c332
kishoreafk <skishoreafk@gmail.com>
2025-07-15 00:56:22 +0530

    Changes

--- projects/script.js
+++ projects/script.js
@@ -41,28 +41,32 @@ function getProjects() {
 
 
 function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
+  let projectsContainer = document.querySelector(".work .box-container");
+  let projectsHTML = "";
+
+  // Show only first project (or filter as you want)
+  let projectsToShow = projects.slice(0, 1); // change number to control how many show
+
+  projectsToShow.forEach(project => {
+    projectsHTML += `
+      <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+          <div class="content">
+            <div class="tag">
+              <h3>${project.name}</h3>
+            </div>
+            <div class="desc">
+              <p>${project.desc}</p>
+              <div class="btns">
+                <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    </div>`
-    });
+      </div>`;
+  });
     projectsContainer.innerHTML = projectsHTML;
 
     // vanilla tilt.js
