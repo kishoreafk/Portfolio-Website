36a0ac94608ccd0715c1146db681c894dc7fe89d
kishoreafk <skishoreafk@gmail.com>
2025-07-15 12:31:53 +0530

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,7 +111,7 @@ function showProjects(projects) {
     projects.slice(0, 1).filter(project => project.category != "android").forEach(project => {
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="/Portfolio-Website/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
