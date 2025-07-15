c82dced4176ae1a1f70e7c3bc93e498b1551bdcb
kishoreafk <skishoreafk@gmail.com>
2025-07-15 10:56:26 +0530

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["AI Engineering", "Model Training","Fine Tuining","Game Development"],
+    strings: ["AI Engineering", "Model Training","Fine Tuning","Game Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
